822b79c532842139d60c1157b00fd94413049e94
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-03-14 14:56:49 +0100

    Add file handler

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String title;
+    private int pages;
+    private String author;
+    private String publisher;
+
+    public Book(String title, int pages, String author, String publisher) throws NumberFormatException, RuntimeException {
+
+        if(title==""||author==""||publisher=="") throw new RuntimeException("Uno o più campi vuoti");
+        if(pages<=0) throw new RuntimeException("Numero pagine <= 0");
+        this.title = title;
+        this.pages = pages;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) throws RuntimeException {
+        if(title=="") throw new RuntimeException("title vuoto");
+        this.title = title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) throws NumberFormatException {
+        if(pages<=0) throw new RuntimeException("Numero pagine <= 0");
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) throws RuntimeException {
+        if(author=="") throw new RuntimeException("author vuoto");
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) throws RuntimeException {
+        if(publisher=="") throw new RuntimeException("publisher vuoto");
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,79 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numberOfBooks = 0;
+
+        String title = "";
+        int pages = 0;
+        String author = "";
+        String publisher = "";
+
+        Scanner scan = new Scanner(System.in);
+
+
+
+        try {
+            System.out.println("Inserisci il numero di libri");
+            numberOfBooks = Integer.parseInt(scan.nextLine());
+            if(numberOfBooks<=0) throw new NumberFormatException("Il numero di libri è minore o uguale a 0");
+
+            Book[] books = new Book[numberOfBooks];
+
+            for (int i=0;i<numberOfBooks;i++) {
+                System.out.print("Titolo: ");
+                title = scan.nextLine();
+                if(title=="") throw new RuntimeException("title vuoto");
+
+                System.out.print("Pagine: ");
+                pages = Integer.parseInt(scan.nextLine());
+                if(pages<=0) throw new NumberFormatException("Il numero di pagine è minore o uguale a 0");
+
+                System.out.print("Autore: ");
+                author = scan.nextLine();
+                if(author=="") throw new RuntimeException("author vuoto");
+
+                System.out.print("Editore: ");
+                publisher = scan.nextLine();
+                if(publisher=="") throw new RuntimeException("publisher vuoto");
+
+                books[i] = new Book(title, pages, author, publisher);
+
+                System.out.println(books[i]);
+
+                File path = new File("./mio-file.txt");
+                FileWriter writer = new FileWriter("mio-file.txt");
+                if(!path.exists()) path.createNewFile();
+                writer.write(books[i].toString());
+                writer.close();
+
+            }
+
+
+            File path = new File("./mio-file.txt");
+            Scanner reader = new Scanner(path);
+            while(reader.hasNextLine()) {
+                String data = reader.nextLine();
+                System.out.println(data);
+            }
+            reader.close();
+
+
+
+
+
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
